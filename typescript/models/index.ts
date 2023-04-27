ba246d3015d7dc53d28e18722f2ca2d103d9e817
MachineUserPTV <github@ptvgroup.com>
2023-04-27 11:18:52 +0000

    Automatic synchronisation from developer.myptv.com

---
+++ typescript/models/index.ts
@@ -0,0 +1,9 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './CausingError';
+export * from './ErrorResponse';
+export * from './Leg';
+export * from './Options';
+export * from './PolylineFormat';
+export * from './Results';
+export * from './RouteResponse';
